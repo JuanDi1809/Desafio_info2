7ed5040d341d8b23ab4bf71799b02483708c96e7
Juan Diego Toro <jtorofranco36@gmail.com>
2024-03-27 16:33:08 -0600

    Cambiando las funciones del modulo CrearCerradura a ConfiguarcionCerraduras

--- Desafio1/ConfigurarCerraduras.cpp
+++ Desafio1/ConfigurarCerraduras.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "CrearMatrices.h"
 
+int dimension = 0;
+bool verifyCenter;
+
 int ***arrayMatrices(int arrayLock[], int dimension){
     int ***matrices = new int**[dimension];
 
@@ -10,3 +13,49 @@ int ***arrayMatrices(int arrayLock[], int dimension){
 
     return matrices;
 }
+
+
+bool comparisonCenterCoord (int fil,int col ){
+    int centerCoord = dimension/2;
+
+    if(fil == col  && fil == centerCoord){
+        return true;
+    }
+
+    return false;
+}
+
+int  **comparisonArray(int **originalMatrix, int **matrixChange, int posComparisonValue, int array[]){
+    //Funcion que compara los valores de las matrices respecto a la clave
+
+    int fil = array[0], col = array[1]; //Fila y columna en donde esta el valor en cada matriz
+    verifyCenter = comparisonCenterCoord(fil, col);
+
+    switch(posComparisonValue){
+
+    case -1:
+        if(originalMatrix[fil][col] < matrixChange[fil][col]){
+            return matrixChange;
+        }
+        return nullptr;
+
+    case 0:
+        if(originalMatrix[fil][col] == matrixChange[fil][col]){
+            return matrixChange;
+        }
+        return nullptr;
+
+    case 1:
+        if(originalMatrix[fil][col] > matrixChange[fil][col]){
+            return matrixChange;
+        }
+        return nullptr;
+
+    default:
+        return nullptr;
+    }
+}
+
+
+
+
--- Desafio1/ConfigurarCerraduras.h
+++ Desafio1/ConfigurarCerraduras.h
@@ -7,4 +7,22 @@
  * @return
  */
 int ***arrayMatrices(int arrayLock[], int dimension);
+
+/**
+ * @brief comparisonArray
+ * @param originalMatrix
+ * @param matrixChange
+ * @param posComparisonValue
+ * @param array
+ * @return
+ */
+int  **comparisonArray(int **originalMatrix, int **matrixChange, int posComparisonValue, int array[]);
+
+/**
+ * @brief comparisonCenterCoord
+ * @param fil
+ * @param col
+ * @return
+ */
+bool comparisonCenterCoord (int fil,int col );
 #endif // CONFIGURARCERRADURAS_H
